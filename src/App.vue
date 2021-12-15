00ba80814ff7e47a83ef435d22985f0713ef058d
Jgashi <40315535+Jgashi@users.noreply.github.com>
2021-12-15 18:15:07 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,27 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="app">
+    <p>hello, {{ name }}</p>
+    <button @click="changeName('Zeld')"></button>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
 
 export default defineComponent({
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: {},
+  data() {
+    return {
+      name: 'Link',
+    };
+  },
+  methods: {
+    changeName(name: string) {
+      this.name = name;
+    },
+  },
 });
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
