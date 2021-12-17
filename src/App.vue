09ce54b587929c84e39c80170fa590df07cac7c9
Jgashi <40315535+Jgashi@users.noreply.github.com>
2021-12-17 10:51:27 +0800

    tutorial

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,53 @@
 <template>
   <div class="app">
-    <p>hello, {{ name }}</p>
-    <button @click="changeName('Zeld')"></button>
+    <JobList :jobs="jobs" />
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, reactive, toRefs, ref } from 'vue';
+import JobList from './components/JobList.vue';
+import Job from './types/Job123';
 
 export default defineComponent({
   name: 'App',
-  components: {},
-  data() {
-    return {
-      name: 'Link',
-    };
-  },
-  methods: {
-    changeName(name: string) {
-      this.name = name;
-    },
-  },
+  components: { JobList },
+  setup() {
+    // const state = reactive({
+    //   name: 'Link',
+    //   age: 25 as string | number
+    // });
+    // state.name = 999 //connot change type
+    // state.age = '26'
+    // return { ...toRefs(state) };
+    // const name = ref('link');
+    // const age = ref<number | string>(25);
+    // return { name, age };
+    const jobs = ref<Job[]>([
+      {
+        title: 'farm worker',
+        location: 'lon lon ranch',
+        salary: 30000,
+        id: '1'
+      },
+      { title: 'quarryman', location: 'death moutain', salary: 40000, id: '2' },
+      {
+        title: 'flute player',
+        location: 'the lost woods',
+        salary: 35000,
+        id: '3'
+      },
+      { title: 'fisherman', location: 'lake hylia', salary: 21000, id: '4' },
+      {
+        title: 'prison guard',
+        location: 'gerudi vally',
+        salary: 32000,
+        id: '5'
+      }
+    ]);
+
+    return { jobs };
+  }
 });
 </script>
 
---
+++ src/components/JobList.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="job-list">
+    <ul>
+      <li v-for="job in jobs" :key="job.id">
+        <h2>{{ job.title }} in {{ job.location }}</h2>
+        <div class="salary">
+          <p>{{ job.salary }} repees</p>
+        </div>
+        <div class="describtion">
+          <p>
+            Lorem, infopqhfiqm wqmdpqwmfdmf fmdf21opfmpovc md1pdo 1m2ep1pd
+            1dmdpoue;lm mndpq mdd qweopm wqeom m[pwdmq[wdhdjcm dq mpo eq dqwopf
+            djpowqd dqpodj
+          </p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue';
+import Job from '@/types/Job123';
+
+export default defineComponent({
+  props: {
+    jobs: {
+      required: true,
+      type: Array as PropType<Job[]>
+    }
+  }
+});
+</script>
+
+<style scoped></style>
